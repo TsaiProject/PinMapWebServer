c0f940cddb23274c25a00f08940b2bd025f75ae7
TsaiProject <tsai.project3321@gmail.com>
2022-05-05 22:29:17 +0800

    Init

---
+++ Controllers/GoogleOAuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PinMapWebServer.Services;
+
+namespace PinMapWebServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoogleOAuthController : ControllerBase
+    {
+        private readonly IGoogleOAuthService _googleOAuthService;
+
+        public GoogleOAuthController(IGoogleOAuthService googleOAuthService)
+        {
+            _googleOAuthService = googleOAuthService;
+
+
+
+        }
+
+        /// <summary>
+        /// User Login
+        /// </summary>
+        [HttpPost]
+        public void Post()
+        {
+            _googleOAuthService.UserLogin();
+        }
+    }
+}
---
+++ Services/GoogleOAuthService.cs
@@ -0,0 +1,34 @@
+﻿using Google.Apis.Auth.OAuth2;
+
+namespace PinMapWebServer.Services
+{
+    public interface IGoogleOAuthService
+    {
+        void UserLogin();
+    }
+
+    public class GoogleOAuthService : IGoogleOAuthService
+    {
+        private readonly ClientSecrets _clientSecrets;
+
+        public GoogleOAuthService(IConfiguration config)
+        {
+            _clientSecrets = new ClientSecrets()
+            {
+                ClientId = config.GetValue<string>("Google.client_id"),
+                ClientSecret = config.GetValue<string>("Google.client_secret")
+            };
+        }
+
+        public void UserLogin()
+        {
+            var user = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                _clientSecrets,
+                new List<string>(),
+                "",
+                CancellationToken.None
+                ).Result;
+
+        }
+    }
+}
